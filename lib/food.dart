c2f6b4bb5270d20e06a90b5e0610064fa0f86dbb
Nidhi Bhanushali <71250816+NidhiBhanushali3701@users.noreply.github.com>
2021-04-05 21:13:55 +0530

    Updated food.dart

--- lib/food.dart
+++ lib/food.dart
@@ -1,8 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fooducate/app_user.dart';
+import 'package:fooducate/calculator_brain.dart';
+import 'package:http/http.dart';
 import 'dart:async';
+import 'api_services.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
-class Food{
-  String name;
-  double quantity,Calorie;
-  var Nutrients = Map();
+class Food {
+  double calories, fat, carbs, protein;
+  String name = '', foodImgURL = '';
+  Food({this.calories, this.fat, this.carbs, this.protein, this.foodImgURL,this.name});
 }
