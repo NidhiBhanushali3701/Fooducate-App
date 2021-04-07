87d739db88c05bd14f920b2c3d8b3906d5bbef41
Nidhi Bhanushali <udemynb3study@gmail.com>
2021-04-07 20:42:11 +0530

    API service added

--- lib/api_services.dart
+++ lib/api_services.dart
@@ -1,5 +1,33 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
-const String apiID = '';
-class apiServices{
+import 'package:convert/convert.dart';
 
+const String apiID = '<apikey>';
+
+class apiServices {
+  apiServices({this.foodItem});
+  String foodItem;
+  String url =
+      'https://api.spoonacular.com/recipes/guessNutrition?apiKey=$apiID&title=';
+  String urlMoreN = 'https://api.spoonacular.com/recipes/1003464/nutritionWidget.json';
+  String imgURL =
+      'https://api.spoonacular.com/recipes/queries/analyze?apiKey=$apiID&q=';
+  Future getData() async {
+    var urlS = url + foodItem;
+    http.Response response = await http.get(urlS);
+    if(response.statusCode==200){
+    print(response.body);
+    var foodData = jsonDecode(response.body);
+    print(foodData);
+    return foodData;}
+    else
+      return {};
+  }
+
+  Future getFoodImg() async {
+    var foodImgURL = imgURL + foodItem;
+    http.Response response = await http.get(foodItem);
+    return jsonDecode(response.body);
+  }
 }
