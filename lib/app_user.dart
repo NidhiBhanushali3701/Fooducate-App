75020b8f488c3ee6ab9a5167d01b1138d3dba909
Nidhi Bhanushali <udemynb3study@gmail.com>
2021-04-08 14:27:06 +0530

    food search & log feature added

--- lib/app_user.dart
+++ lib/app_user.dart
@@ -23,7 +23,7 @@ class AppUser {
   gender _gender;
   List<String> MedCond = new List(5);
   List<String> FoodPref = new List(5);
-  List<Food> _food = List();//List.filled(500,Food(foodImgURL: ' ',carbs: 0,calories: 0,name: ' ',fat: 0,protein: 0,quantity: 1),growable: true);
+  List<Food> _food = List();
   Goal _goal, _goalPerWeek;
   //Progress progress;
   //CalculatorBrain cAppUserCalculatorBrain;
