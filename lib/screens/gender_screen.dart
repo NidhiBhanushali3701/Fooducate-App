8b4387f9fcd3be86fdfec39a6c6a2c8867c0b440
Nidhi Bhanushali <udemynb3study@gmail.com>
2021-04-13 12:57:14 +0530

    backend updated

--- lib/screens/gender_screen.dart
+++ lib/screens/gender_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fooducate/app_user.dart';
 import 'package:fooducate/reusable_card.dart';
@@ -5,14 +6,14 @@ import 'package:fooducate/constants.dart';
 import 'package:fooducate/icon_content.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:fooducate/calculate_button.dart';
-import 'file:///C:/Users/Nidhi/Desktop/AndroidStudioProjects/fooducate/lib/screens/user_data_input_screen.dart';
+import 'user_data_input_screen.dart';
 
 //enum gender { female, male }
 
 class GenderSelect extends StatefulWidget {
   static String id = 'genderSelectScreen';
   AppUser cAppUser;
-  GenderSelect({Key key,@required this.cAppUser}) : super(key: key);
+  GenderSelect({Key key, @required this.cAppUser}) : super(key: key);
   @override
   _GenderSelectState createState() => _GenderSelectState();
 }
@@ -20,6 +21,33 @@ class GenderSelect extends StatefulWidget {
 class _GenderSelectState extends State<GenderSelect> {
   var _gender;
   AppUser cAppUser;
+  final _fireBaseStore = FirebaseFirestore.instance;
+  String cAppUserEmail;
+  void updateUserDataInFireBaseStore(String updateField, var updatedValue) {
+    _fireBaseStore
+        .collection('clients')
+        .doc(cAppUserEmail)
+        .update({updateField: updatedValue});
+  }
+
+  void getUserDataFromFireBaseStore() async {
+    var appUsers = await _fireBaseStore.collection('clients').get();
+    for (var appUser in appUsers.docs) {
+      var appUserData = appUser.data();
+      print(appUserData);
+      if (cAppUserEmail == appUserData['email']) {
+        print('init state $appUserData');
+        _gender = appUserData['gender'];
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getUserDataFromFireBaseStore();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Map arguments = ModalRoute.of(context).settings.arguments as Map;
@@ -30,6 +58,7 @@ class _GenderSelectState extends State<GenderSelect> {
       //final cAppUser = arguments['CurrentAppUserData'];
       cAppUser = arguments['CurrentAppUserData'];
       print('in BMI calc ${cAppUser.getEmail()},${cAppUser.getGender()}');
+      cAppUserEmail = cAppUser.getEmail();
     }
     return Scaffold(
       appBar: AppBar(
@@ -37,67 +66,110 @@ class _GenderSelectState extends State<GenderSelect> {
         title: Text('USER PROFILE'),
         centerTitle: true,
       ),
-      body: SafeArea(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(9.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Expanded(
-                  child: ReusableCard(
-                    onPress: () {
-                      _gender = gender.male;
-                      print('male');
-                      cAppUser.setGender(gender.male);
-                      print('male');
-                      Navigator.pushReplacementNamed(context, UserData.id,arguments: {'CurrentAppUserData': cAppUser});
-                      setState(() {
-                        _gender = gender.male;
-                        print('male');
-                        cAppUser.setGender(gender.male);
-                        Navigator.pushReplacementNamed(context, UserData.id,arguments: {'CurrentAppUserData': cAppUser});
-                      });
-                    },
-                    colour: _gender == gender.male
-                        ? kActiveCardColour
-                        : kInactiveCardColour,
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.mars,
-                      label: '  MALE  ',
-                    ),
+      body: FutureBuilder(
+          future: _fireBaseStore.collection('clients').get(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              // If we got an error
+              if (snapshot.hasData) {
+                for (var appUsers in snapshot.data.docs) {
+                  if (appUsers['email'] == cAppUserEmail) {
+                    print(appUsers['gender']);
+                    cAppUser.setGender(appUsers['gender']);
+                    print(appUsers['gender']);
+                    break;
+                  }
+                }
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    '${snapshot.error} occured',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                );
+              }
+            }
+            /*else {
+              return CircularProgressIndicator(
+                backgroundColor: kInactiveCardColour,
+              );
+            }*/
+            return SafeArea(
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(9.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Expanded(
+                        child: ReusableCard(
+                          onPress: () {
+                            _gender = gender.male;
+                            print('male');
+                            updateUserDataInFireBaseStore(
+                                'gender', gender.male.toString());
+                            cAppUser.setGender(gender.male);
+                            print('male');
+                            Navigator.pushReplacementNamed(context, UserData.id,
+                                arguments: {'CurrentAppUserData': cAppUser});
+                            setState(() {
+                              _gender = gender.male;
+                              print('male');
+                              updateUserDataInFireBaseStore(
+                                  'gender', gender.male.toString());
+                              cAppUser.setGender(gender.male);
+                              Navigator.pushReplacementNamed(
+                                  context, UserData.id,
+                                  arguments: {'CurrentAppUserData': cAppUser});
+                            });
+                          },
+                          colour: _gender == gender.male
+                              ? kActiveCardColour
+                              : kInactiveCardColour,
+                          cardChild: IconContent(
+                            icon: FontAwesomeIcons.mars,
+                            label: '  MALE  ',
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: ReusableCard(
+                          onPress: () {
+                            _gender = gender.female;
+                            print('female');
+                            updateUserDataInFireBaseStore(
+                                'gender', gender.female.toString());
+                            cAppUser.setGender(gender.female);
+                            print('female');
+                            Navigator.pushReplacementNamed(context, UserData.id,
+                                arguments: {'CurrentAppUserData': cAppUser});
+                            setState(() {
+                              _gender = gender.female;
+                              print('female');
+                              updateUserDataInFireBaseStore(
+                                  'gender', gender.female.toString());
+                              cAppUser.setGender(gender.female);
+                              Navigator.pushReplacementNamed(
+                                  context, UserData.id,
+                                  arguments: {'CurrentAppUserData': cAppUser});
+                            });
+                          },
+                          colour: _gender == gender.female
+                              ? kActiveCardColour
+                              : kInactiveCardColour,
+                          cardChild: IconContent(
+                            icon: FontAwesomeIcons.venus,
+                            label: ' FEMALE ',
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                Expanded(
-                  child: ReusableCard(
-                    onPress: () {
-                      _gender = gender.female;
-                      print('female');
-                      cAppUser.setGender(gender.female);
-                      print('female');
-                      Navigator.pushReplacementNamed(context, UserData.id,arguments: {'CurrentAppUserData': cAppUser});
-                      setState(() {
-                        _gender = gender.female;
-                        print('female');
-                        cAppUser.setGender(gender.female);
-                        Navigator.pushReplacementNamed(context, UserData.id,arguments: {'CurrentAppUserData': cAppUser});
-                      });
-                    },
-                    colour: _gender == gender.female
-                        ? kActiveCardColour
-                        : kInactiveCardColour,
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.venus,
-                      label: ' FEMALE ',
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+              ),
+            );
+          }),
     );
   }
 }
